2ad7734e959f83cdc9fdd7fb2fae495cde3ece0d
vioif <ireneaflores3@gmail.com>
2017-12-13 20:39:55 -0500

    finished excercise 6

--- excercise6.rb
+++ excercise6.rb
@@ -1,3 +1,12 @@
 puts "What is the temperature in Fahrenheit?"
 
 temp = gets.to_i
+
+def convert (fahrenheitconvert)
+
+  fahrenheitconvert = (f - 32) * 5 / 9
+end
+
+final_temperature = convert(fahrenheitconvert)
+
+puts "Your temperature would be #{final_temperature} degrees celsius!"
